090e9bd53e50e5978adcde21b44e1a8ae4f2c3a6
Aaquib111 <aaquibahm@gmail.com>
2025-03-01 09:15:25 +0000

    init

---
+++ vj-work/.ipynb_checkpoints/gen-checkpoint.py
@@ -0,0 +1,28 @@
+from vllm import LLM, SamplingParams
+import torch
+
+prompts = [
+    "<think>\nWhat is the capital of France?\n"
+]
+
+sampling_params = SamplingParams(
+    temperature=0.8,
+    top_p=0.95
+)
+
+# Configure for 8 H100s
+llm = LLM(
+    model="deepseek-ai/DeepSeek-R1-Distill-Qwen-32B",
+    tensor_parallel_size=8,
+    max_model_len=1024,
+    gpu_memory_utilization=0.8,
+    enforce_eager=True,
+    dtype=torch.bfloat16,
+)
+
+outputs = llm.generate(prompts, sampling_params)
+
+for output in outputs:
+    prompt = output.prompt
+    generated_text = output.outputs[0].text
+    print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
